58e8743a845426c47d2b70cdd697962f79cdafbe
martinlosak <martinlosak2@gmail.com>
2016-03-13 01:10:48 +0100

    Initial commit

---
+++ app/src/main/java/com/losak/chirpcoin/MainActivity.java
@@ -0,0 +1,172 @@
+package com.losak.chirpcoin;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.coinbase.android.sdk.OAuth;
+import com.coinbase.api.exception.CoinbaseException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import io.chirp.sdk.CallbackCreate;
+import io.chirp.sdk.CallbackRead;
+import io.chirp.sdk.ChirpSDK;
+import io.chirp.sdk.ChirpSDKListener;
+import io.chirp.sdk.model.Chirp;
+import io.chirp.sdk.model.ChirpError;
+import io.chirp.sdk.model.ShortCode;
+
+public class MainActivity extends AppCompatActivity {
+    private ChirpSDK chirpSDK;
+    private TextView textView1;
+
+    String API_KEY = "dvd019NCpaUCWn8TeIH96lX6B";
+    String API_SECRET = "rPUMGtsPsn6ucH0qnLB0Bh33u3pVi3Xj36zWYxZzeo99lIsSXB";
+    String CLIENT_ID = "5b86f05a70970ac9932055ae65b4cfdd63c25b6a52a53d67a31420153378374a";
+    String CLIENT_SECRET = "hJ1WUVPlRwztNOX1PVNB24swiGKlQ1vn";
+    String REDIRECT_URI = "chirpcoin://coinbase-oauth";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        FloatingActionButton floatingActionButton = (FloatingActionButton) findViewById(R.id.fab);
+        textView1 = (TextView) findViewById(R.id.textView1);
+        final TextView textView2 = (TextView) findViewById(R.id.textView2);
+        Button button = (Button)findViewById(R.id.button);
+
+        setSupportActionBar(toolbar);
+
+        //Coinbase
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    OAuth.beginAuthorization(MainActivity.this, CLIENT_ID, "user", REDIRECT_URI, null);
+                    textView2.setText("Success");
+                } catch (CoinbaseException e) {
+                    e.getStackTrace();
+                }
+            }
+        });
+
+
+        //ChirpCoin
+        chirpSDK = new ChirpSDK(this, API_KEY, API_SECRET);
+        chirpSDK.setListener(new ChirpSDKListener() {
+
+            @Override
+            public void onChirpHeard(ShortCode shortCode) {
+                chirpSDK.read(shortCode, new CallbackRead() {
+                    @Override
+                    public void onReadResponse(final Chirp chirp) {
+//                        try {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                textView1.setText(chirp.getJsonData().toString());
+                            }
+                        });
+                        Log.e("onReadResponse", chirp.getJsonData().toString());
+
+//                        } catch (JSONException e) {
+//                            e.printStackTrace();
+//                        }
+                    }
+
+                    @Override
+                    public void onReadError(ChirpError chirpError) {
+                        Log.e("onReadError", chirpError.getMessage());
+                    }
+                });
+            }
+
+            @Override
+            public void onChirpError(ChirpError chirpError) {
+                Log.e("onChirpError", chirpError.getMessage());
+            }
+        });
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                JSONObject jsonObj = new JSONObject();
+                try {
+                    jsonObj.put("amount", "127");
+                    jsonObj.put("currency", "btc");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                Chirp chirp = new Chirp(jsonObj);
+                chirpSDK.create(chirp, new CallbackCreate() {
+
+                    @Override
+                    public void onCreateResponse(ShortCode shortCode) {
+                        chirpSDK.play(shortCode);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(MainActivity.this, "Chirp sent", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+                    }
+
+                    @Override
+                    public void onCreateError(ChirpError chirpError) {
+                        Toast.makeText(MainActivity.this, "" + chirpError.getCode(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        chirpSDK.startListening();
+        Log.e("onResume", "startListening");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        chirpSDK.stopListening();
+        Log.e("onPause", "stopListening");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
